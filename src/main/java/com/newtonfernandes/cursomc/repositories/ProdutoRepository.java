75e4da2c904065fabc12d8d0abab2d3ce620fcb3
Newton Fernandes <newtonfernandesti@gmail.com>
2021-10-14 14:12:21 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/newtonfernandes/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.newtonfernandes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.newtonfernandes.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
