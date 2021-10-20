00a1808de0db88fe7ff6ba89e225a46733c56c52
Newton Fernandes <newtonfernandesti@gmail.com>
2021-10-20 10:08:06 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/newtonfernandes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/newtonfernandes/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,11 @@ public class CategoriaResource {
 		obj.setId(id);
 		obj = categoriaService.update(obj);
 		return ResponseEntity.status(HttpStatus.OK).body(obj);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
+		categoriaService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/newtonfernandes/cursomc/services/CategoriaService.java
+++ src/main/java/com/newtonfernandes/cursomc/services/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.newtonfernandes.cursomc.domain.Categoria;
 import com.newtonfernandes.cursomc.repositories.CategoriaRepository;
+import com.newtonfernandes.cursomc.services.exceptions.DataIntegrityException;
 import com.newtonfernandes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,5 +36,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return categoriaRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		}catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}	
 	}
 }
