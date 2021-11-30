f48c752058b1849d72e2c7941cd871f81e5ed5ac
Newton Fernandes <newtonfernandesti@gmail.com>
2021-11-30 14:49:30 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/newtonfernandes/cursomc/resources/PedidoResource.java
+++ src/main/java/com/newtonfernandes/cursomc/resources/PedidoResource.java
@@ -3,6 +3,7 @@ package com.newtonfernandes.cursomc.resources;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newtonfernandes.cursomc.domain.Pedido;
@@ -21,7 +23,7 @@ public class PedidoResource {
 
 	@Autowired
 	private PedidoService pedidoService;
-		
+			
 	@GetMapping("/{id}")
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = pedidoService.buscar(id);
@@ -32,5 +34,15 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> insert(@Valid @RequestBody Pedido obj){
 		obj = pedidoService.insert(obj);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="instante") String orderBy, 
+			@RequestParam(value="direction", defaultValue="DESC") String direction) {
+		Page<Pedido> list = pedidoService.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/newtonfernandes/cursomc/services/PedidoService.java
+++ src/main/java/com/newtonfernandes/cursomc/services/PedidoService.java
@@ -5,9 +5,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.newtonfernandes.cursomc.domain.Cliente;
 import com.newtonfernandes.cursomc.domain.ItemPedido;
 import com.newtonfernandes.cursomc.domain.PagamentoComBoleto;
 import com.newtonfernandes.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.newtonfernandes.cursomc.domain.enums.EstadoPagamento;
 import com.newtonfernandes.cursomc.repositories.ItemPedidoRepository;
 import com.newtonfernandes.cursomc.repositories.PagamentoRepository;
 import com.newtonfernandes.cursomc.repositories.PedidoRepository;
+import com.newtonfernandes.cursomc.security.UserSpringSecurity;
+import com.newtonfernandes.cursomc.services.exceptions.AuthorizationException;
 import com.newtonfernandes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -40,7 +46,7 @@ public class PedidoService {
 	
 	@Autowired
 	private EmailService emailService;
-			
+		
 	public Pedido buscar(Integer id) {
 		Optional<Pedido> obj = pedidoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -72,4 +78,15 @@ public class PedidoService {
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, 
+			String direction){	
+		UserSpringSecurity user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+	}
 }
